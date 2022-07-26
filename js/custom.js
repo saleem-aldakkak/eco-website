8ae0b94515a098326e41f6282ccf2cd96d52a24e
ar4-space <107211999+ar4-space@users.noreply.github.com>
2022-07-26 10:03:54 +0200

    language update

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,83 @@
+/*-----------------------------------------------------------------------------------
+
+    Theme Name: Crizal - Multipurpose Responsive Template + Admin
+    Description: Multipurpose Website Template + Admin + RTL
+    Author: sd
+    Version: 3.1
+
+    /* ----------------------------------
+
+    JS Active Code Index
+            
+        01. Handle href links on smartphone
+        02. Localisation
+        
+    ---------------------------------- */  
+
+/*------------------------------------
+    01. Handle href links on smartphone
+--------------------------------------*/
+$( "header li" ).click(function() {
+            if($(this).has('ul')){
+                if($(this).children('ul').height()!=0){
+                    
+                    window.location.href = $(this).children('a').attr('href');
+                }
+               
+            }
+            
+});
+
+
+/*------------------------------------
+    02. Localisation
+--------------------------------------*/
 
 
 
-$("#en-btn").click(function(event) {
-    event.preventDefault();
+
+//Read Query
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+
+// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+let lang = params.lang; // "some_value"
+
+if($.cookie('lang')==undefined){
+  lang=cookie('lang');
+}else{
+  cookie('lang','en');
+}
+if(lang!=null){
+// Get Languages
+fetch("lang/lang.json")
+.then(function(response) {
+  return response.json();
+})
+.then(function(result) {
+  //Reorder lang buttons
+  $('#selected-lang').attr('href','?lang='+lang);
+  $('#selected-lang').html('<img class="lang-img" src="'+result[lang]['img']+'"/><span langid="'+result[lang]['langid']+'"></span>');
+  $('#hidden-langs').html("");
+   for(let i in result){
+     if(i!=lang){
+       $('#hidden-langs').append('<li><a href="?lang='+i+'"><img class="lang-img" src="'+result[i].img+'"><span langid="'+result[i].langid+'"></span></a></li>');
+     }
     
-  });
-
-  $("#de-btn").click(function(event) {
-    event.preventDefault();
+   }
     
-   
-  });
-//Select Language de,en
-var lang = "en"
+});
+$.cookie('lang', lang);
+}
 
-// Read Languages from de-en.json
+
+
+
+
+
+
+// Read Language's text from de-en.json
 fetch("lang/de-en.json")
 .then(function(response) {
   return response.json();
@@ -28,7 +91,9 @@ fetch("lang/de-en.json")
                 break;
                 case "de":
                     $('*[langid="'+result[i].ID+'"]').text(result[i].German);
-                    break;
+                break;
+            default:
+              break;
         }
         
        
